5dd29e98ea37fd95676f2972fe65c4af81a92966
maxzetina <maxzetina4@gmail.com>
2023-06-20 20:59:08 -0400

    Initial Commit

---
+++ LaCasa/LaCasaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaCasaApp.swift
+//  LaCasa
+//
+//  Created by Maxwell Zetina on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct LaCasaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
